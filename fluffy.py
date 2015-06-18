ff2da4cae6b6dd841e8eff97634f5fe5939e4481
Samuel Lucas <smlucas13@gmail.com>
2015-06-17 20:49:43 -0400

    save

--- fluffy.py
+++ fluffy.py
@@ -1,6 +1,39 @@
+
 import colors as c
 import random as r
+import random
+
 
 """ The pink fluffy unicorn quiz module."""
 
 
+print(c.clear)
+print(c.base3 + 'samuel lucas is not responsible for anything in this quiz.' + c.reset)
+print(c.yellow + 'welcome to the quiz game for unicorn hunters.' + c.reset)
+
+
+def q1():
+    print(c.red  + 'What is the color of the pink fluffy unicorn?')
+    answer == input('> ').strip().lower()
+    if answer.startswith("pink"):
+        return True
+    else:
+        return False
+
+def q2():
+    print(c.red + 'What is the pink fluffy unicorn dancing on?')
+    answer == input('> ').strip().lower()
+    if answer.startswith("rainbow"):
+        return True
+    else:
+        return False
+
+def q3():
+    print(c.red + 'How do we describe the pink fluffy unicorns fur?')
+    answer == input('> ').strip().lower()
+    if answer.startswith("smile"):
+        return True
+    else:
+        return False
+
+questions = [q1,q2,q3]
--- util.py
+++ util.py
@@ -5,14 +5,30 @@ import colors as c
 from colors import *
 from util import ask
 
+"""---------------------------------------"""
+
 def ask(question,color=c.yellow):
     print(c.red  + 'What is the color of the pink fluffy unicorn?')
     pink = input('> ').strip().lower()
     print(c.reset)
     return answer
 
+"""---------------------------------------"""
+
 if __main__ == '__main__':
     print(c.clear)
     name = ask("what is your name?")
     color = ask ("what is your name in color?",c.random_color())
+
+"""--------------------------------------"""
+
+questions = [q1,qi2,q3]
+
+while questions:
+    question = random.choice(questions)
+    answer = question()
+    if answer:
+        questions.remove(question)
+
+"""-----------------------------------"""
 
