83fbc4edadcd39d3e44a22f3fdcf8ca14ecfb4fa
Samuel Lucas <smlucas13@gmail.com>
2015-07-15 20:18:16 -0400

    save

--- util.py
+++ util.py
@@ -1,34 +1,21 @@
-#!/usr/bin/env python3
 """This is the chest its got junk and treasures so get out."""
 
 import colors as c
-from colors import *
-from util import ask
 
 """---------------------------------------"""
 
-def ask(question,color=c.yellow):
-    print(c.red  + 'What is the color of the pink fluffy unicorn?')
-    pink = input('> ').strip().lower()
+def ask(question):
+    print(question)
+    answer = input('> ').strip().lower()
     print(c.reset)
     return answer
 
 """---------------------------------------"""
 
-if __main__ == '__main__':
+if __name__ == '__main__':
     print(c.clear)
     name = ask("what is your name?")
     color = ask ("what is your name in color?",c.random_color())
-
-"""--------------------------------------"""
-
-questions = [q1,qi2,q3]
-
-while questions:
-    question = random.choice(questions)
-    answer = question()
-    if answer:
-        questions.remove(question)
 
 """-----------------------------------"""
 
